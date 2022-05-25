f7592a4931b84713ef511ff0699cd6f864bf3777
yinghao <2354949236@qq.com>
2022-05-25 15:24:12 +0800

    fff

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!sss</a>
+          Welcome to <a href="https://nextjs.org">Next.js!s</a>
         </h1>
 
         <p className={styles.description}>

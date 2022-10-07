65830c836ecbefd967cfb049a8b2479ddceb3540
yinghao <2354949236@qq.com>
2022-10-07 23:54:20 +0800

    fix: tsconfig

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '@styles/Home.module.scss'
 import classNames from "classnames/bind";
 
 export default function Home() {

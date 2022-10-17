fbe4344b3429d8d69c022e0b713162f556862b28
yinghao <2354949236@qq.com>
2022-10-17 18:38:03 +0800

    prettier

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,22 +1,23 @@
 import '@styles/globals.css'
 import type {AppProps} from 'next/app'
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import Head from "next/head";
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import Head from 'next/head'
+
 export default function MyApp({Component, pageProps}: AppProps) {
-    if (typeof window !== "undefined") {
-        require("/public/iconfont");
-    }
+  if (typeof window !== 'undefined') {
+    require('/public/iconfont')
+  }
 
-    const faviconArr = ['吸尘器.svg','收音机.svg','电脑.svg','电视机.svg','空调.svg']
-    const num = parseInt(String(Math.random() * 100))%faviconArr.length
+  const faviconArr = ['吸尘器.svg', '收音机.svg', '电脑.svg', '电视机.svg', '空调.svg']
+  const num = parseInt(String(Math.random() * 100)) % faviconArr.length
 
-    return <>
-        <Head>
-            <title>Welcome to YH Park</title>
-            <link rel="icon" href={'/favicon/'+ faviconArr[num]} />
-        </Head>
-        <Component {...pageProps} />
-    </>
+  return <>
+    <Head>
+      <title>Welcome to YH Park</title>
+      <link rel="icon" href={'/favicon/' + faviconArr[num]}/>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -1,19 +1,19 @@
 import styles from '@styles/Home.module.scss'
-import classNames from "classnames/bind";
-import {useRouter} from "next/router";
+import classNames from 'classnames/bind'
+import {useRouter} from 'next/router'
 
 export default function Home() {
-    const cn = classNames.bind(styles)
-    const router = useRouter()
+  const cn = classNames.bind(styles)
+  const router = useRouter()
 
-    return (
-        <div className={styles.container}>
-            <p>now is 10-17</p>
-            <p>try to run actions</p>
-            <div className={cn('card')} onClick={() => router.push('/knowledgeRouter')}>
-                <p>Knowledge</p>
-            </div>
-
+  return (
+      <div className={styles.container}>
+        <p>now is 10-17</p>
+        <p>try to run actions</p>
+        <div className={cn('card')} onClick={() => router.push('/knowledgeRouter')}>
+          <p>Knowledge</p>
         </div>
-    )
+
+      </div>
+  )
 }

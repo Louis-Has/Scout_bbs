9e20d7b7e7fc3b726bc21cb1536acc42ea51b314
yinghao <2354949236@qq.com>
2022-11-15 00:27:37 +0800

    feat: Nav+

--- next-app/src/pages/GameOrder/index.tsx
+++ next-app/src/pages/GameOrder/index.tsx
@@ -1,7 +1,12 @@
+import Nav from '@components/Nav'
 import React, { useEffect } from 'react'
 
 const GameOrder: React.FC = () => {
-  return <div>ssd</div>
+  return (
+    <>
+      <Nav />
+    </>
+  )
 }
 
 export default GameOrder

e80c0d6a7a7a76f70d5d1cd0d2cef6b290441c60
yinghao <2354949236@qq.com>
2023-03-17 16:16:54 +0800

    feat: 17 ready to shs

--- nest-service/src/app.module.ts
+++ nest-service/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MongoConfigModel } from './config/config.module';
 import { CaseModule } from './case/case.module';
 import { APP_FILTER } from '@nestjs/core';
+import { ArticleModule } from './article/article.module'
 
 @Module({
-  imports: [MongoConfigModel, CaseModule],
+  imports: [MongoConfigModel, CaseModule,ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
